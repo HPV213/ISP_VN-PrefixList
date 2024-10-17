3782db705d90503206980b5816d09901ee2096b2
HUYNH PHUOC VIET <HPV213@gmail.com>
2024-10-17 22:23:09 +0700

    Create prefix-list.sh
    Tạo CLI prefix-list IP VN

---
+++ prefix-list.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+curl -s --header 'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -L http://thongkeinternet.vn/jsp/vnix/danhba_ipasn.jsp -o output.txt
+if [[ $? -ne 0 ]]; then
+	echo "Error: Curl command failed"
+else
+	egrep -o '[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+/[0-9]+' output.txt | while read IP; do
+    echo "ip prefix-list ipv4_VN $IP permit" 
+    done > ipv4_CIDR-prefix-list_VN
+ 	#egrep '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' output.txt | sed 's/[[:space:]]//g' > ip-vnnic-v6
+fi
